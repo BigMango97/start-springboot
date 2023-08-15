987fe9adf0a03dd081c3eb4e117dec99fa4006ca
minkyeong kim <mgk04222@naver.com>
2023-08-15 22:55:22 +0900

    롬복을 활용해 리팩터링하기

--- src/main/java/project/project/controller/ArticleController.java
+++ src/main/java/project/project/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package project.project.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import project.project.entity.Article;
 
 import project.project.repository.ArticleRepository;
 
+@Slf4j //로깅 기능을 위한 어노테이션 추가
 @Controller
 public class ArticleController {
     @Autowired
@@ -22,10 +24,12 @@ public class ArticleController {
         System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString()); //DTO가 엔티티로 잘 변환되는지 확인 출력
-         // 2. 리파지터리로 엔티티를 DB에 저장
+        log.info(form.toString());
+        //System.out.println(article.toString()); //DTO가 엔티티로 잘 변환되는지 확인 출력
+        // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString()); //article이 DB에 잘 저장되는지 확인 출력s
+        log.info(saved.toString());
+        //System.out.println(saved.toString()); //article이 DB에 잘 저장되는지 확인 출력s
         return "";
     }
 }
