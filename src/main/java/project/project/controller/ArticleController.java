79a1ac772365815519e838e224c723382e4da9f7
minkyeong kim <mgk04222@naver.com>
2023-08-17 19:43:26 +0900

    데이터 목록 조회하기

--- src/main/java/project/project/controller/ArticleController.java
+++ src/main/java/project/project/controller/ArticleController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 
 import project.project.repository.ArticleRepository;
 
+import java.util.ArrayList;
+
 @Slf4j //로깅 기능을 위한 어노테이션 추가
 @Controller
 public class ArticleController {
@@ -43,5 +45,14 @@ public class ArticleController {
         model.addAttribute("article",articleEntity);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){ // 1. model 객체 받아오기
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleEntityList",articleEntityList); // 2. articleEntityList 등록
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- src/main/java/project/project/repository/ArticleRepository.java
+++ src/main/java/project/project/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package project.project.repository;
 import org.springframework.data.repository.CrudRepository;
 import project.project.entity.Article;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
