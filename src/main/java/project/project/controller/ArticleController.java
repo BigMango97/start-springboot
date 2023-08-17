c9b23748cd59475075610befe62fe2f13a4b999b
minkyeong kim <mgk04222@naver.com>
2023-08-17 19:17:54 +0900

    단일 데이터 조회하기

--- src/main/java/project/project/controller/ArticleController.java
+++ src/main/java/project/project/controller/ArticleController.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import project.project.dto.ArticleForm;
 import project.project.entity.Article;
+import org.springframework.ui.Model;
 
 import project.project.repository.ArticleRepository;
 
@@ -14,7 +16,7 @@ import project.project.repository.ArticleRepository;
 @Controller
 public class ArticleController {
     @Autowired
-    private ArticleRepository articleRepository;
+    private ArticleRepository articleRepository; // -> 리파지터리 구현객체 주입받기
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -31,5 +33,15 @@ public class ArticleController {
         log.info(saved.toString());
         //System.out.println(saved.toString()); //article이 DB에 잘 저장되는지 확인 출력s
         return "";
+    }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){ //id를 매개변수로 가져오기
+        log.info("id =" + id); //id를 잘 가져왔는지 확인하기
+        // 1. id를 조회해 데이터를 가져오기 -> DB에서 데이터를 가져오는 주체 : 리파지터리
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터를 등록하기
+        model.addAttribute("article",articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
