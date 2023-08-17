e0f74eab33b35d635eb455aec408e27a04e1df5b
minkyeong kim <mgk04222@naver.com>
2023-08-17 21:21:42 +0900

    수정 페이지 만들기

--- src/main/java/project/project/controller/ArticleController.java
+++ src/main/java/project/project/controller/ArticleController.java
@@ -51,8 +51,19 @@ public class ArticleController {
         // 1. 모든 데이터 가져오기
         ArrayList<Article> articleEntityList = articleRepository.findAll();
         // 2. 모델에 데이터 등록하기
-        model.addAttribute("articleEntityList",articleEntityList); // 2. articleEntityList 등록
+        model.addAttribute("articleList",articleEntityList); // 2. articleEntityList 등록
         // 3. 뷰 페이지 설정하기
         return "articles/index";
     }
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id,Model model){ //모델 객체 받아오기
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity); //articleEntity를 article로 등록
+        //뷰 페이지 설정하기
+        return "articles/edit";
+    }
+
 }
